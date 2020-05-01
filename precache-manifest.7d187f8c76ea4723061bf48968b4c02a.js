f2fc34d6a873f0e93572271798c2e82dfed4ddf2
Tejas <56431505+tejasvtanna@users.noreply.github.com>
2020-05-01 21:20:55 +0530

    Updates

---
+++ precache-manifest.7d187f8c76ea4723061bf48968b4c02a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48d4ab12d3a22087cb8e744eb309ef26",
+    "url": "/expense-tracker-context/index.html"
+  },
+  {
+    "revision": "c2b8c5744dc41ca653cd",
+    "url": "/expense-tracker-context/static/css/main.3e67108d.chunk.css"
+  },
+  {
+    "revision": "dd4904e024a43740879d",
+    "url": "/expense-tracker-context/static/js/2.0925b119.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/expense-tracker-context/static/js/2.0925b119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2b8c5744dc41ca653cd",
+    "url": "/expense-tracker-context/static/js/main.a3926a75.chunk.js"
+  },
+  {
+    "revision": "552c5863bca0b4af536d",
+    "url": "/expense-tracker-context/static/js/runtime-main.aa842a82.js"
+  }
+]);
